feec338e8a4a381190dc0b978a81268e1baeb8e1
IceColdOros <chad.kriell@gmail.com>
2025-09-27 09:55:23 +0200

    main logic v1

--- Bots/MyFirstBot.java
+++ Bots/MyFirstBot.java
@@ -1,4 +1,29 @@
 import dev.robocode.tankroyale.botapi.*;
+import dev.robocode.tankroyale.botapi.events.*;
 
-public class MyFirstBot extends Bot {
+//main method
+public static void main(String[]args) {
+    new MyFirstBot9.start();
+}
+
+MyFirstBot() {
+    super(BotInfo..fromfile("MyFirstBot.json"));
+}
+
+// Called when a new round is started -> initialize and do some movement
+@Override
+public void run() {
+    // Repeat while the bot is running
+    while (isRunning()) {
+        forward(100);
+        turnGunRight(360);
+        back(100);
+        turnGunRight(360);
+    }
+}
+
+// we see another bot, fire!
+@Override
+public void onScannedBot(ScannedBotEvent e) {
+    fire(1)
 }
