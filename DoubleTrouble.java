0c7be9239975b8f4eb7e265c16ae1dd4e27d04db
IceColdOros <chad.kriell@gmail.com>
2025-09-27 16:01:03 +0200

    The Actual LOCK

--- DoubleTrouble.java
+++ DoubleTrouble.java
@@ -1,58 +1,79 @@
+//STARTING BOT
+
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import dev.robocode.tankroyale.botapi.graphics.Color;
 
-// ------------------------------------------------------------------
-// SpinBot
-// ------------------------------------------------------------------
-// A sample bot original made for Robocode by Mathew Nelson.
-//
-// Continuously moves in a circle while firing at maximum power when
-// detecting enemies.
-// ------------------------------------------------------------------
-public class SpinBot extends Bot {
+public class DoubleTrouble extends Bot {
 
+    int turnDirection = 1;
     // The main method starts our bot
     public static void main(String[] args) {
-        new SpinBot().start();
+        new DoubleTrouble().start();
     }
 
     // Called when a new round is started -> initialize and do some movement
     @Override
     public void run() {
         setBodyColor(Color.BLACK);
-        setTurretColor(Color.RED);
+        setTracksColor(Color.RED);
         setRadarColor(Color.GREEN);
-        setScanColor(Color.YELLOW);
 
-        // Repeat while the bot is running
+
         while (isRunning()) {
-            // Tell the game that when we take move, we'll also want to turn right... a lot
-            setTurnRight(10_000);
-            // Limit our speed to 5
-            setMaxSpeed(5);
-            // Start moving (and turning)
-            forward(10_000);
+            turnGunRight(10);
         }
     }
 
-    // We scanned another bot -> fire hard!
+    // We saw another bot -> fire!
     @Override
     public void onScannedBot(ScannedBotEvent e) {
-        fire(3);
-    }
+        turnToFaceTarget(e.getX(), e.getY());
+        var bearingFromGun = gunBearingTo(e.getX(), e.getY());
+        turnGunLeft(bearingFromGun);
 
-    // We hit another bot -> if it's our fault, we'll stop turning and moving,
-    // so we need to turn again to keep spinning.
+        if (Math.abs(bearingFromGun) <= 3 && getGunHeat() == 0) {
+            fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+        }
+        var distance = distanceTo(e.getX(), e.getY());
+        forward(distance + 5);
+
+    }
     @Override
     public void onHitBot(HitBotEvent e) {
-        var direction = directionTo(e.getX(), e.getY());
-        var bearing = calcBearing(direction);
-        if (bearing > -10 && bearing < 10) {
+        turnToFaceTarget(e.getX(), e.getY());
+
+        // Determine a shot that won't kill the bot...
+        // We want to ram him instead for bonus points
+        if (e.getEnergy() > 16) {
             fire(3);
+        } else if (e.getEnergy() > 10) {
+            fire(2);
+        } else if (e.getEnergy() > 4) {
+            fire(1);
+        } else if (e.getEnergy() > 2) {
+            fire(.5);
+        } else if (e.getEnergy() > .4) {
+            fire(.1);
         }
-        if (e.isRammed()) {
-            turnRight(10);
+    }
+
+    // We were hit by a bullet -> turn perpendicular to the bullet
+    @Override
+    public void onHitByBullet(HitByBulletEvent e) {
+        // Calculate the bearing to the direction of the bullet
+        double bearing = calcBearing(e.getBullet().getDirection());
+
+        // Turn 90 degrees to the bullet direction based on the bearing
+        turnLeft(90 - bearing);
+    }
+    private void turnToFaceTarget(double x, double y) {
+        var bearing = bearingTo(x, y);
+        if (bearing >= 0) {
+            turnDirection = 1;
+        } else {
+            turnDirection = -1;
         }
+        turnLeft(bearing);
     }
 }
